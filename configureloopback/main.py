ec0fc698a5b7443e7317dc2f877f3708f7abc394
PriyaaNandhakumar <priyaa1027@gmail.com>
2023-09-28 11:19:56 +0530

    "Configure/Create/Delete/Show Loopback Interface - Tested"

--- configureloopback/main.py
+++ configureloopback/main.py
@@ -15,6 +15,22 @@ import re
 #Create a Flask application
 app = Flask(__name__)
 
+
+########  Supporting Functions for Routes #########
+def connectionstring(data):
+    hostname = data["hostname"]
+    username = data["username"]
+    password = data["password"]
+    
+    device_params = {
+    'device_type': 'cisco_xr',
+    'ip': hostname,
+    'username': username,
+    'password': password,
+    }
+    
+    return device_params
+
 def configure_loopback_interface(device_params, interface_number, ip_address):
     try:
         # Connect to the IOS XR device
@@ -39,28 +55,91 @@ def configure_loopback_interface(device_params, interface_number, ip_address):
     except Exception as e:
         return {'message': str(e)}, 500
 
-       
+def show_loopback_interface(device_params,interface_number):
+    try:
+        # Connect to the IOS XR device
+        net_connect = ConnectHandler(**device_params)
+        net_connect.enable()
+
+        # Construct the CLI command
+        command = f'show running-config interface Loopback{interface_number}'
+
+        # Send the command to the device
+        output = net_connect.send_command(command)
+
+        # Close the SSH connection
+        net_connect.disconnect()
+
+        return {'message': 'Interface configuration:', 'output': output}, 200
+
+    except Exception as e:
+        return {'message': str(e)}, 500
+        
+        
+def delete_loopback_interface(device_params,interface_number):
+    try:
+        # Connect to the IOS XR device
+        net_connect = ConnectHandler(**device_params)
+        net_connect.enable()
+
+        # Construct the CLI command to delete the loopback interface
+        delete_command = [
+            f'configure terminal',
+            f'no interface Loopback{interface_number}',
+            'commit',
+            'end',
+        ]
+
+        # Send the delete command to the device
+        output = net_connect.send_config_set(delete_command)
+
+        # Close the SSH connection
+        net_connect.disconnect()
+
+        return {'message': 'Interface deleted successfully.', 'output': output}, 200
+
+    except Exception as e:
+        return {'message': str(e)}, 500        
+
+ ##############################   Routes  #################################
+
+ 
 @app.route('/configure_loopback', methods=['POST'])
 def configure_loopback_route():
     data = request.json
-    hostname = data.get('hostname')
-    username = data.get('username')
-    password = data.get('password')
+    device_params=connectionstring(data)
     loopback_ip = data.get('loopback_ip')
     interface_number = data.get('interface_number')
     
-    device_params = {
-    'device_type': 'cisco_xr',
-    'ip': hostname,
-    'username': username,
-    'password': password,
-    }
-
     result = configure_loopback_interface(device_params, interface_number, loopback_ip)
 
     return jsonify({'result': result})
 
 
+@app.route('/show_loopback', methods=['POST'])
+def show_loopback():
+    data = request.json
+    device_params=connectionstring(data)
+    interface_number = data.get('interface_number')
+    
+    if not interface_number:
+        return {'message': 'interface_number is required as a query parameter.'}, 400
+    
+    result, status_code = show_loopback_interface(device_params,interface_number)
+    return jsonify(result), status_code
+    
+    
+@app.route('/delete_loopback', methods=['POST'])
+def delete_loopback():
+    data = request.get_json()
+    device_params=connectionstring(data)
+    interface_number = data.get('interface_number')
+    
+    if not interface_number:
+        return {'message': 'interface_number is required.'}, 400
+    
+    result, status_code = delete_loopback_interface(device_params,interface_number)
+    return jsonify(result), status_code    
     
 if __name__ == '__main__':
     app.run(debug=True)
